e8043eb36f00f073de7490e3d9c493748a2f20a5
Yaroslav Pankratyev <yaroslav.pankratyev@xored.com>
2017-05-27 17:32:08 +0700

    pom.xml, start implementing UI

---
+++ src/main/java/com/pankratyev/jetbrains/filebrowser/App.java
@@ -0,0 +1,32 @@
+package com.pankratyev.jetbrains.filebrowser;
+
+import com.pankratyev.jetbrains.filebrowser.ui.Browser;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.WindowConstants;
+
+/**
+ * Entry point.
+ */
+public final class App {
+    private static final String WINDOW_TITLE = "File browser with preview";
+    private static final int WINDOW_WIDTH = 800;
+    private static final int WINDOW_HEIGHT = 600;
+
+    public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException,
+            InstantiationException, IllegalAccessException {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        SwingUtilities.invokeLater(() -> createFrame().setVisible(true));
+    }
+
+    private static JFrame createFrame() {
+        JFrame frame = new JFrame(WINDOW_TITLE);
+        frame.setContentPane(new Browser().getMainContainer());
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        return frame;
+    }
+}
---
+++ src/main/java/com/pankratyev/jetbrains/filebrowser/ui/Browser.java
@@ -0,0 +1,19 @@
+package com.pankratyev.jetbrains.filebrowser.ui;
+
+import javax.swing.*;
+
+/**
+ *
+ */
+public final class Browser {
+    private JPanel mainContainer;
+    private JSplitPane splitPane;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+
+    public JPanel getMainContainer() {
+        return mainContainer;
+    }
+}
