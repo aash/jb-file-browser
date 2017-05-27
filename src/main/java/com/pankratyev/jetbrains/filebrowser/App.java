69376d3132593639cb82a9419cdad7321d23f592
Yaroslav Pankratyev <yaroslav.pankratyev@xored.com>
2017-05-27 19:56:54 +0700

    Configure pom.xml to build executable jar

--- src/main/java/com/pankratyev/jetbrains/filebrowser/App.java
+++ src/main/java/com/pankratyev/jetbrains/filebrowser/App.java
@@ -19,7 +19,12 @@ public final class App {
     public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException,
             InstantiationException, IllegalAccessException {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        SwingUtilities.invokeLater(() -> createFrame().setVisible(true));
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                createFrame().setVisible(true);
+            }
+        });
     }
 
     private static JFrame createFrame() {
