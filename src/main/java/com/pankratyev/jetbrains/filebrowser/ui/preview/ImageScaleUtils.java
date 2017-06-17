23c29f1759ad58bcfa0b15c03b202fcf925b6a79
Yaroslav Pankratyev <yaroslav.pankratyev@xored.com>
2017-06-17 16:29:11 +0700

    Save aspect ratio on image resize

--- src/main/java/com/pankratyev/jetbrains/filebrowser/ui/preview/ImageScaleUtils.java
+++ src/main/java/com/pankratyev/jetbrains/filebrowser/ui/preview/ImageScaleUtils.java
@@ -7,15 +7,31 @@ final class ImageScaleUtils {
     private ImageScaleUtils() {
     }
 
+    /**
+     * Resizes an image if it's width and/or height is greater than the maximal one. Aspect ratio is saved.
+     */
     static Image resizeIfNecessary(BufferedImage image, int maxWidth, int maxHeight) {
-        int previewWidth = image.getWidth();
-        int previewHeight = image.getHeight();
+        int originalWidth = image.getWidth();
+        int originalHeight = image.getHeight();
 
         Image resultImage = image;
-        if (previewWidth > maxWidth || previewHeight > maxHeight) {
-            //TODO save width/height ratio
-            resultImage = image.getScaledInstance(maxWidth, maxHeight, Image.SCALE_DEFAULT);
+        if (originalWidth > maxWidth || originalHeight > maxHeight) {
+            int newWidth = originalWidth;
+            int newHeight = originalHeight;
+
+            if (originalWidth > maxWidth) {
+                newWidth = maxWidth;
+                newHeight = (newWidth * originalHeight) / originalWidth;
+            }
+
+            if (newHeight > maxHeight) {
+                newHeight = maxHeight;
+                newWidth = (newHeight * originalWidth) / originalHeight;
+            }
+
+            resultImage = image.getScaledInstance(newWidth, newHeight, Image.SCALE_FAST);
         }
+
         return resultImage;
     }
 }
