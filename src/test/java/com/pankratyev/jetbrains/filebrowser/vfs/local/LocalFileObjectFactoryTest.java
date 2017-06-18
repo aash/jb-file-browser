161257ef5b4d19e1adb9ad6c24bfff6bd89f4b67
Yaroslav Pankratyev <yaroslav.pankratyev@xored.com>
2017-06-18 22:45:14 +0700

    Fix LocalFileObjectFactoryTest

--- src/test/java/com/pankratyev/jetbrains/filebrowser/vfs/local/LocalFileObjectFactoryTest.java
+++ src/test/java/com/pankratyev/jetbrains/filebrowser/vfs/local/LocalFileObjectFactoryTest.java
@@ -5,34 +5,39 @@ import org.junit.Test;
 
 import java.nio.file.Paths;
 
-import static org.junit.Assert.assertNotNull;
+import static java.io.File.separator;
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public final class LocalFileObjectFactoryTest {
     @Test
     public void testCreateByStr() {
-        LocalFileObject res = LocalFileObjectFactory.create("/a/b/c/");
+        LocalFileObject res = LocalFileObjectFactory.create(
+                separator + "a" + separator + "b" + separator + "c" + separator);
         verifyResult(res);
     }
 
     @Test
     public void testCreateByPath() {
-        LocalFileObject res = LocalFileObjectFactory.create(Paths.get("/a/b/c/"));
+        LocalFileObject res = LocalFileObjectFactory.create(Paths.get(
+                separator + "a" + separator + "b" + separator + "c" + separator));
         verifyResult(res);
     }
 
     private static void verifyResult(LocalFileObject fileObject) {
         assertEquals("c", fileObject.getName());
-        assertEquals("/a/b/c", fileObject.getFullName());
+        assertTrue(fileObject.getFullName(),
+                fileObject.getFullName().endsWith(separator + "a" + separator + "b" + separator + "c"));
 
         FileObject parent1 = fileObject.getParent();
         assertNotNull(parent1);
         assertEquals("b", parent1.getName());
-        assertEquals("/a/b", parent1.getFullName());
+        assertTrue(parent1.getFullName(), parent1.getFullName().endsWith(separator + "a" + separator + "b"));
 
         FileObject parent2 = parent1.getParent();
         assertNotNull(parent2);
         assertEquals("a", parent2.getName());
-        assertEquals("/a", parent2.getFullName());
+        assertTrue(parent2.getFullName(), parent2.getFullName().endsWith(separator + "a"));
     }
 }
